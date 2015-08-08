e0aba4593b5c9b82b183f8f761de94d0217a2678
Ahkari <gqz825584053@qq.com>
2015-08-08 14:19:02 +0800

    animaSearch静态页编写

---
+++ js/libs/datepicker/config.js
@@ -0,0 +1,13 @@
+﻿//�����б�
+var langList =
+[
+	{ name: 'en', charset: 'UTF-8' },
+	{ name: 'zh-cn', charset: 'gb2312' },
+	{ name: 'zh-tw', charset: 'GBK' }
+];
+
+//Ƥ���б�
+var skinList =
+[
+    { name: 'twoer', charset: 'gb2312' }
+];
